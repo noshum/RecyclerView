3184dc63db78ee4976b51f62a455de1edceabbb6
Noshum <m.o.shumov@gmail.com>
2021-10-04 04:05:45 +0300

    dev

---
+++ app/src/main/java/ua/shumov/reminder/activity/LogoActivity.kt
@@ -0,0 +1,32 @@
+package ua.shumov.reminder.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import ua.shumov.reminder.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this,
+            R.anim.splash_in
+        ))
+        Handler().postDelayed({
+            ic_logo.startAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.splash_out
+            ))
+            Handler().postDelayed({
+                ic_logo.visibility = View.GONE
+                startActivity(Intent(this@LogoActivity, DashboardActivity::class.java))
+                finish()
+            }, 500)
+        }, 1500)
+    }
+}
